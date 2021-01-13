992249732c8ff38dcec1c947ff9a67291e6a4f31
dkssyddico <dkssyddico@naver.com>
2021-01-13 21:04:38 +0900

    update

--- app.js
+++ app.js
@@ -9,15 +9,21 @@ import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 import globalRouter from './routers/globlalRouter';
 import routes from './routes';
+
 const app = express();
 
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use(function (req, res, next) {
   res.setHeader('Content-Security-Policy', "script-src 'self' https://archive.org");
   return next();
 });
 app.set('view engine', 'pug');
 app.use('/uploads', express.static('uploads'));
+app.use('/static', express.static('static'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
