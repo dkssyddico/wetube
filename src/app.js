067bb7c14ad736ed51c5239205deef735fab77e4
dkssyddico <dkssyddico@naver.com>
2021-02-26 20:37:09 +0900

    feb 26th

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill';
 import 'core-js';
 import express from 'express';
 import morgan from 'morgan';
@@ -7,6 +8,7 @@ import bodyParser from 'body-parser';
 import passport from 'passport';
 import mongoose from 'mongoose';
 import session from 'express-session';
+import path from 'path';
 import MongoStore from 'connect-mongo';
 import { localsMiddleware } from './middlewares';
 import userRouter from './routers/userRouter';
@@ -31,8 +33,8 @@ app.use(function (req, res, next) {
   return next();
 });
 app.set('view engine', 'pug');
-app.use('/uploads', express.static('uploads'));
-app.use('/static', express.static('static'));
+app.set('views', path.join(__dirname, 'views'));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
